57319f1296a917d82b6a3942c3c07f737a1e0e0b
Hazem Abou El-Fadl <hazemaboullfadl@gmail.com>
2024-03-01 18:14:53 +0200

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Hazem Abou El Fadl on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
